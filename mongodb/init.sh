185c3069bd8ee709e38067e635220a241829b83c
Martin Čorovčák <martino.coro@gmail.com>
2023-10-31 14:30:53 +0100

    Add mongoDB schema

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mongoimport --db ecommerce --collection vendors --drop --file ./data/vendor.json --jsonArray | tee -a logs/import.log
+mongoimport --db ecommerce --collection orders --drop --file ./data/orders.json --jsonArray | tee -a logs/import.log
 
-mongoimport --db ecommerce --collection products --drop --file ./data/product.json --jsonArray | tee -a logs/import.log
+mongoimport --db ecommerce --collection people --drop --file ./data/people.json --jsonArray | tee -a logs/import.log
+
+mongoimport --db ecommerce --collection vendors --drop --file ./data/vendors.json --jsonArray | tee -a logs/import.log
