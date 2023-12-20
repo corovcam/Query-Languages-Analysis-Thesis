0e5099eba60ccc515220e71eefd37fe6c74145db
Martin Čorovčák <martino.coro@gmail.com>
2023-12-20 22:47:15 +0000

    Update mongo import

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -2,10 +2,16 @@
 
 timestamp=$(date +"%Y-%m-%d_%s")
 
-data_dir="data_old"
+data_dir="data_1k"
 
-for file in "$data_dir"/*.json; do
-    fullFilename=$(basename "$file")
-    filename=${fullFilename%.*}
-    mongoimport --db ecommerce --collection "$filename" --drop --file "$file" --jsonArray |& tee -a logs/import_"$timestamp".log
-done
+if [ -n "$(ls "$data_dir"/*.bson 2>/dev/null)" ]
+then
+    # Contains BSON files from running `mongodump -d ecommerce -o "$data_dir"`
+    mongorestore -d ecommerce "$data_dir" |& tee -a logs/import_"$timestamp".log
+else
+    for file in "$data_dir"/*.json; do
+        fullFilename=$(basename "$file")
+        filename=${fullFilename%.*}
+        mongoimport --db ecommerce --collection "$filename" --drop --file "$file" --jsonArray |& tee -a logs/import_"$timestamp".log
+    done
+fi
