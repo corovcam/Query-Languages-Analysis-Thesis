8645f6ef0065e60f34abb17f92e27e781abd8e97
Martin Čorovčák <martino.coro@gmail.com>
2023-12-21 15:10:31 +0100

    Update Cassandra query files

--- cassandra/run_queries.sh
+++ cassandra/run_queries.sh
@@ -2,16 +2,24 @@
 
 set -euo pipefail
 
+record_volume=1000
+
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 log_file="logs/query_$timestamp.log"
+query_csv_file=logs/queries/query_results_"$timestamp".csv
 
-cqlsh -e "TRUNCATE system_traces.sessions;" | tee -a "$log_file"
+echo "db,record_volume,query,iteration,time_in_seconds" | tee -a "$query_csv_file"
 
-for i in {1..20}
-do
-    echo "Iteration $i, started at $(date +"%Y-%m-%d %T")" | tee -a "$log_file"
-    # Setting request-timeout to 300 seconds = 5 minutes
-    cqlsh -f ./queries/query.cql --request-timeout=300 | tee -a "$log_file"
+for file in queries/testing/*.cql; do
+    fullFilename=$(basename "$file")
+    filename=${fullFilename%.*}
+    for i in {1..20}
+    do
+        printf "cassandra,%s,%s,%s," "$record_volume" "$filename" "$i" | tee -a "$query_csv_file"
+        cqlsh -e "TRUNCATE system_traces.sessions;" |& tee -a "$log_file"
+        echo "Iteration $i, started at $(date +"%Y-%m-%d %T")" |& tee -a "$log_file"
+        # Setting request-timeout to 300 seconds = 5 minutes
+        cqlsh -k ecommerce -f "$file" --request-timeout=300 |& tee -a "$log_file"
+        cqlsh -e "SELECT duration FROM system_traces.sessions;" 2>> "$log_file" | sed -n '4p' | tr -d " \t"  | tee -a "$query_csv_file"
+    done
 done
-
-cqlsh -e "COPY system_traces.sessions TO 'logs/queries/sessions_$timestamp.csv' WITH HEADER = TRUE;" | tee -a "$log_file"
