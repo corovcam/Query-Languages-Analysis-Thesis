a507295b0fbe96e98616b911b57be3d6270b5fc5
Martin Čorovčák <martino.coro@gmail.com>
2024-04-10 15:23:36 +0000

    Refactor table names in count_table_rows.sql

--- common/count_table_rows.sql
+++ common/count_table_rows.sql
@@ -1,47 +1,47 @@
 SELECT "Customer" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Customer`
+FROM `Customer`
 UNION
 SELECT "Industry" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Industry`
+FROM `Industry`
 UNION
 SELECT "Order" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Order`
+FROM `Order`
 UNION
 SELECT "Order_Contacts" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Order_Contacts`
+FROM `Order_Contacts`
 UNION
 SELECT "Order_Products" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Order_Products`
+FROM `Order_Products`
 UNION
 SELECT "Person" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Person`
+FROM `Person`
 UNION
 SELECT "Person_Person" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Person_Person`
+FROM `Person_Person`
 UNION
 SELECT "Person_Tags" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Person_Tags`
+FROM `Person_Tags`
 UNION
 SELECT "Post" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Post`
+FROM `Post`
 UNION
 SELECT "Post_Tags" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Post_Tags`
+FROM `Post_Tags`
 UNION
 SELECT "Product" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Product`
+FROM `Product`
 UNION
 SELECT "Tag" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Tag`
+FROM `Tag`
 UNION
 SELECT "Type" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Type`
+FROM `Type`
 UNION
 SELECT "Vendor" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Vendor`
+FROM `Vendor`
 UNION
 SELECT "Vendor_Contacts" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Vendor_Contacts`
+FROM `Vendor_Contacts`
 UNION
 SELECT "Vendor_Products" AS table_name, COUNT(*) AS exact_row_count
-FROM `ecommerce`.`Vendor_Products`;
+FROM `Vendor_Products`;
